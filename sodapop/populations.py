be82e7b91661ebe89ec8a03c5c35773c133c45ce
Phil <philippe.landry@ligo.org>
2021-04-06 12:22:07 -0400

    fix bns norms

--- sodapop/populations.py
+++ sodapop/populations.py
@@ -100,14 +100,14 @@ def bimodcut_mass(m,mu1=1.34,sigma1=0.07,mu2=1.80,sigma2=0.21,alpha=0.65,mmin=0.
 def unif_m1m2(m1,m2,mmin=5.,mmax=2e2): # uniform distribution in source frame masses, subject to m1 >= m2 convention
 
 	if m1 < m2 or m1 > mmax or m2 < mmin: val = 0.
-	else: val = 1./(mmax-mmin)**2
+	else: val = 2./(mmax-mmin)**2
     
 	return val
 	
 def peak_m1m2(m1,m2,mu=1.34,sigma=0.02): # gaussian distribution in source frame masses, subject to m1 >= m2 convention
 
 	if m1 < m2: val = 0.
-	else: val = gaussian(m1,mu,sigma)*gaussian(m2,mu,sigma)
+	else: val = 2.*gaussian(m1,mu,sigma)*gaussian(m2,mu,sigma)
 	
 	return val
 	
@@ -117,7 +117,7 @@ def bimod_m1m2(m1,m2,mu1=1.34,sigma1=0.02,mu2=1.47,sigma2=0.15,alpha=0.68): # do
 	else:
 		norm1 = 1.
 		norm2 = 1.
-		val = (alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*(alpha*gaussian(m2,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m2,mu2,sigma2)/norm2)
+		val = 2.*(alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*(alpha*gaussian(m2,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m2,mu2,sigma2)/norm2)
 	
 	return val
 	
@@ -126,7 +126,7 @@ def peakcut_m1m2(m1,m2,mu=1.34,sigma=0.02,mmin=1.,mmax=3.): # gaussian distribut
 	if m1 < m2 or m1 > mmax or m2 < mmin: val = 0.
 	else:
 		norm = 0.5*(scipy.special.erf((mmax-mu)/(np.sqrt(2.)*sigma))-scipy.special.erf((mmin-mu)/(np.sqrt(2.)*sigma)))
-		val = gaussian(m1,mu,sigma)*gaussian(m2,mu,sigma)/norm**2
+		val = 2.*gaussian(m1,mu,sigma)*gaussian(m2,mu,sigma)/norm**2
 	
 	return val
 	
@@ -136,7 +136,7 @@ def bimodcut_m1m2(m1,m2,mu1=1.34,sigma1=0.07,mu2=1.80,sigma2=0.21,alpha=0.65,mmi
 	else:
 		norm1 = 0.5*(scipy.special.erf((mmax-mu1)/(np.sqrt(2)*sigma1))-scipy.special.erf((mmin-mu1)/(np.sqrt(2)*sigma1)))
 		norm2 = 0.5*(scipy.special.erf((mmax-mu2)/(np.sqrt(2)*sigma2))-scipy.special.erf((mmin-mu2)/(np.sqrt(2)*sigma2)))
-		val = (alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*(alpha*gaussian(m2,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m2,mu2,sigma2)/norm2)
+		val = 2.*(alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*(alpha*gaussian(m2,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m2,mu2,sigma2)/norm2)
 	
 	return val
 	
@@ -145,7 +145,7 @@ def peakcut_m1_unif_m2(m1,m2,mu=1.34,sigma=0.02,mmin=1.,mmax=3.): # gaussian dis
 	if m1 < m2 or m1 > mmax or m2 < mmin: val = 0.
 	else:
 		norm = 0.5*(scipy.special.erf((mmax-mu)/(np.sqrt(2)*sigma))-scipy.special.erf((mmin-mu)/(np.sqrt(2)*sigma)))
-		val = gaussian(m1,mu,sigma)*unif_mass(m2,mmin,mmax)/norm
+		val = 2.*gaussian(m1,mu,sigma)*unif_mass(m2,mmin,mmax)/norm
 	
 	return val
 	
@@ -155,7 +155,7 @@ def bimodcut_m1_unif_m2(m1,m2,mu1=1.34,sigma1=0.02,mu2=1.47,sigma2=0.15,alpha=0.
 	else:
 		norm1 = 0.5*(scipy.special.erf((mmax-mu1)/(np.sqrt(2)*sigma1))-scipy.special.erf((mmin-mu1)/(np.sqrt(2)*sigma1)))
 		norm2 = 0.5*(scipy.special.erf((mmax-mu2)/(np.sqrt(2)*sigma2))-scipy.special.erf((mmin-mu2)/(np.sqrt(2)*sigma2)))
-		val = (alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*unif_mass(m2,mmin,mmax)
+		val = 2.*(alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*unif_mass(m2,mmin,mmax)
 	
 	return val
 	
@@ -173,7 +173,7 @@ def peakcut_m1m2_qpair(m1,m2,mu=1.34,sigma=0.02,mmin=1.,mmax=3.,beta=0.): # gaus
 	if m1 < m2 or m1 > mmax or m2 < mmin: val = 0.
 	else:
 		norm = scipy.integrate.dblquad(lambda m1,m2 : gaussian(m1,mu,sigma)*gaussian(m2,mu,sigma)*(m2/m1)**beta, mmin, mmax, mmin, mmax)[0]
-		val = gaussian(m1,mu,sigma)*gaussian(m2,mu,sigma)*(m2/m1)**beta/norm
+		val = 2.*gaussian(m1,mu,sigma)*gaussian(m2,mu,sigma)*(m2/m1)**beta/norm
 	
 	return val
 	
@@ -186,7 +186,7 @@ def bimodcut_m1m2_qpair(m1,m2,mu1=1.34,sigma1=0.07,mu2=1.80,sigma2=0.21,alpha=0.
 			
 		norm = scipy.integrate.dblquad(lambda m1,m2 : (alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*(alpha*gaussian(m2,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m2,mu2,sigma2)/norm2)*(m2/m1)**beta, mmin, mmax, mmin, mmax)[0]
 			
-		val = (alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*(alpha*gaussian(m2,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m2,mu2,sigma2)/norm2)*(m2/m1)**beta/norm
+		val = 2.*(alpha*gaussian(m1,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m1,mu2,sigma2)/norm2)*(alpha*gaussian(m2,mu1,sigma1)/norm1 + (1.-alpha)*gaussian(m2,mu2,sigma2)/norm2)*(m2/m1)**beta/norm
 	
 	return val
 	
@@ -212,7 +212,7 @@ def o3a_powerpeak_m1_unif_m2(m1,m2,mu=1.34,sigma=0.02,mmin=1.,mmax=3.):
 	if m1 < m2 or m2 > mmax or m2 < mmin: val = 0.
 	else:
 		norm = 0.5*(scipy.special.erf((mmax-mu)/(np.sqrt(2)*sigma))-scipy.special.erf((mmin-mu)/(np.sqrt(2)*sigma)))
-		val = ((1.-lpeak)*powerlaw(m1,-alpha,mmin,mmax)+lpeak*gaussian(m1,mu,sigma)/norm)*smooth(m1,mmin,delta)*unif_mass(m2,mmin,mmax)
+		val = 2.*((1.-lpeak)*powerlaw(m1,-alpha,mmin,mmax)+lpeak*gaussian(m1,mu,sigma)/norm)*smooth(m1,mmin,delta)*unif_mass(m2,mmin,mmax)
 	
 	return val
 	
