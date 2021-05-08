69c746bd2ad079ff4697e6671256a79637e4bc8d
Phil <philippe.landry@ligo.org>
2021-05-07 21:48:33 -0400

    fix prior string bug

--- bin/sample-pop-params.sh
+++ bin/sample-pop-params.sh
@@ -5,6 +5,6 @@ numsamps=$2
 priorstr=$3
 outpath=$4
 
-IFS=' ' read -r -a prior <<< "$priorstr"
+IFS='+' read -r -a prior <<< "$priorstr"
 
 sample-pop-params $popmodel -n $numsamps -p ${prior[@]} -o $outpath -v
