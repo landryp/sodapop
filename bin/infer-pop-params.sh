33ddc14de32b53264cc1803f31150f28b83faed6
Phil <philippe.landry@ligo.org>
2021-05-11 13:29:05 -0400

    trim unused option

--- bin/infer-pop-params.sh
+++ bin/infer-pop-params.sh
@@ -6,16 +6,15 @@ colstr=$3
 likesamps=$4
 popmodelstr=$5
 priorstr=$6
-popsamps=$7
-bhparams=$8
-selectfunc=$9
-selectpriorstr=${10}
-selectsamps=${11}
-numpost=${12}
-numwalkers=${13}
-numburnin=${14}
-outpath=${15}
-batch=${16}
+bhparams=$7
+selectfunc=$8
+selectpriorstr=$9
+selectsamps=${10}
+numpost=${11}
+numwalkers=${12}
+numburnin=${13}
+outpath=${14}
+batch=${15}
 
 IFS=',' read -r -a like <<< "$likestr"
 IFS=',' read -r -a col <<< "$colstr"
@@ -24,4 +23,4 @@ IFS='=' read -r -a prior <<< "$priorstr"
 IFS=',' read -r -a bh <<< "$bhparams"
 IFS='+' read -r -a selprior <<< "$selectpriorstr"
 
-infer-pop-params $popparams ${like[@]} -c ${col[@]} -l $likesamps -p ${popmod[@]} -P ${prior[@]} -n $popsamps -B ${bh[@]} -f $selectfunc -S ${selprior[@]} -s $selectsamps -t $numpost -w $numwalkers -b $numburnin -o $outpath --batch $batch -v
+infer-pop-params $popparams ${like[@]} -c ${col[@]} -l $likesamps -p ${popmod[@]} -P ${prior[@]} -B ${bh[@]} -f $selectfunc -S ${selprior[@]} -s $selectsamps -t $numpost -w $numwalkers -b $numburnin -o $outpath --batch $batch -v
