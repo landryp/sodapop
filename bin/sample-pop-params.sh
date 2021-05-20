8f3022a252ffe516c92c186cfefbcd94012622d6
Phil <philippe.landry@ligo.org>
2021-05-19 23:08:24 -0400

    update vars for sample-pop-params exec

--- bin/sample-pop-params.sh
+++ bin/sample-pop-params.sh
@@ -6,7 +6,7 @@ priorstr=$3
 fixedstr=$4
 outpath=$5
 
-IFS='+' read -r -a prior <<< "$priorstr"
+IFS='=' read -r -a prior <<< "$priorstr"
 IFS='+' read -r -a fixed <<< "$fixedstr"
 
 sample-pop-params $popmodel -n $numsamps -p ${prior[@]} -F ${fixed[@]} -o $outpath -v
